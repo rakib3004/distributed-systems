df96310f0282d51f7fb207ca8e5d95a528540430
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2022-06-22 18:47:30 +0600

    add hello world function

---
+++ REST.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import ast
+app = Flask(__name__)
+api = Api(app)
+
+class Helloworld(Resource):
+
+	def __init__(self):
+
+		pass
+
+	def get(self):
+
+		return {
+
+			"Hello": "World"
+
+		}
+
+api.add_resource(Helloworld, '/')
+
+if __name__ == '__main__':
+
+	app.run(debug=True)
