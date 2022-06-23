5ec4f70e727f7d6b5a212db1db809b65216d8bd1
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2022-06-23 22:49:12 +0600

    rst-api-nodejs-distributed-systems-add-services

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,27 +1,41 @@
+// In src/controllers/workoutController.js
+// *** ADD ***
+const workoutService = require("../services/workoutService");
+
 const getAllWorkouts = (req, res) => {
-    res.send("Get all workouts");
-  };
-  
-  const getOneWorkout = (req, res) => {
-    res.send("Get an existing workout");
-  };
-  
-  const createNewWorkout = (req, res) => {
-    res.send("Create a new workout");
-  };
-  
-  const updateOneWorkout = (req, res) => {
-    res.send("Update an existing workout");
-  };
-  
-  const deleteOneWorkout = (req, res) => {
-    res.send("Delete an existing workout");
-  };
-  
-  module.exports = {
-    getAllWorkouts,
-    getOneWorkout,
-    createNewWorkout,
-    updateOneWorkout,
-    deleteOneWorkout,
-  };
+  // *** ADD ***
+  const allWorkouts = workoutService.getAllWorkouts();
+  res.send("Get all workouts");
+};
+
+const getOneWorkout = (req, res) => {
+  // *** ADD ***
+  const workout = workoutService.getOneWorkout();
+  res.send("Get an existing workout");
+};
+
+const createNewWorkout = (req, res) => {
+  // *** ADD ***
+  const createdWorkout = workoutService.createNewWorkout();
+  res.send("Create a new workout");
+};
+
+const updateOneWorkout = (req, res) => {
+  // *** ADD ***
+  const updatedWorkout = workoutService.updateOneWorkout();
+  res.send("Update an existing workout");
+};
+
+const deleteOneWorkout = (req, res) => {
+  // *** ADD ***
+  workoutService.deleteOneWorkout();
+  res.send("Delete an existing workout");
+};
+
+module.exports = {
+  getAllWorkouts,
+  getOneWorkout,
+  createNewWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
+};
---
+++ src/services/workoutService.js
@@ -0,0 +1,28 @@
+// In src/services/workoutService.js
+const getAllWorkouts = () => {
+    return;
+  };
+  
+  const getOneWorkout = () => {
+    return;
+  };
+  
+  const createNewWorkout = () => {
+    return;
+  };
+  
+  const updateOneWorkout = () => {
+    return;
+  };
+  
+  const deleteOneWorkout = () => {
+    return;
+  };
+  
+  module.exports = {
+    getAllWorkouts,
+    getOneWorkout,
+    createNewWorkout,
+    updateOneWorkout,
+    deleteOneWorkout,
+  };
