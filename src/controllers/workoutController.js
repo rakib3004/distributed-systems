4df452cdd6e573e75886c9e9a390ed78af73e647
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2022-06-24 10:52:30 +0600

    modify-workout.js

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -12,10 +12,7 @@ const getOneWorkout = (req, res) => {
   res.send("Get an existing workout");
 };
 
-const createNewWorkout = (req, res) => {
-  const createdWorkout = workoutService.createNewWorkout();
-  res.send("Create a new workout");
-};
+
 
 const updateOneWorkout = (req, res) => {
   const updatedWorkout = workoutService.updateOneWorkout();
@@ -26,6 +23,35 @@ const deleteOneWorkout = (req, res) => {
   workoutService.deleteOneWorkout();
   res.send("Delete an existing workout");
 };
+
+
+
+const createNewWorkout = (req, res) => {
+  const { body } = req;
+  // *** ADD ***
+  if (
+    !body.name ||
+    !body.mode ||
+    !body.equipment ||
+    !body.exercises ||
+    !body.trainerTips
+  ) {
+    return;
+  }
+  // *** ADD ***
+  const newWorkout = {
+    name: body.name,
+    mode: body.mode,
+    equipment: body.equipment,
+    exercises: body.exercises,
+    trainerTips: body.trainerTips,
+  };
+  // *** ADD ***
+  const createdWorkout = workoutService.createNewWorkout(newWorkout);
+  // *** ADD ***
+  res.status(201).send({ status: "OK", data: createdWorkout });
+};
+
 
 module.exports = {
   getAllWorkouts,
--- src/database/Workout.js
+++ src/database/Workout.js
@@ -1,7 +1,27 @@
+// In src/database/Workout.js
 const DB = require("./db.json");
+// *** ADD ***
+const { saveToDatabase } = require("./utils");
+
 
 const getAllWorkouts = () => {
   return DB.workouts;
 };
 
-module.exports = { getAllWorkouts };
+// *** ADD ***
+const createNewWorkout = (newWorkout) => {
+  const isAlreadyAdded =
+    DB.workouts.findIndex((workout) => workout.name === newWorkout.name) > -1;
+  if (isAlreadyAdded) {
+    return;
+  }
+  DB.workouts.push(newWorkout);
+  saveToDatabase(DB);
+  return newWorkout;
+};
+
+module.exports = {
+  getAllWorkouts,
+  // *** ADD ***
+  createNewWorkout,
+};
