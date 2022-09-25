64f4ca69a95413b9df4cee492e3547f969f2184e
rakib3004 <bsse1129@iit.du.ac.bd>
2022-09-25 10:30:34 +0600

    add new students and backup studenst

--- MiniYouTube/Videos/app.js
+++ MiniYouTube/Videos/app.js
@@ -1,28 +1,42 @@
 const express = require('express');
 const app = express();
-const PORT =2040;
+const PORT =1129;
 
 app.use(express.json())
 
-let data = [
-    {
-        name: "Introduction to Docker",
-        playlist: "Distributed Tutorial"
-    },
-    {
-        name: "Docker Swarm in 200 secs",
-        playlist: "Crash Turorial"
-    }
-]
+let data = ["Rakib","Muktar","Rahat","Siam","Inzamam"]
 
-app.get('/videos', (req,res)=>{
+app.get('/students', (req,res)=>{
 
     res.json(data);
 })
 
 
+app.get('/backupStudents', (req,res)=>{
 
-app.post('/videos/:id', (req,res)=>{
+    var drive_data = JSON.stringify(data);
+
+    var fs = require('fs')
+
+    var file = fs.createWriteStream('BSSE.txt');
+
+    file.on('error',function(error){
+        res.status(418).send({message: 'Failed to backup'})
+    });
+
+    data.forEach(function(v){
+        file.write(v)
+
+        file.end();
+
+    })
+
+
+
+})
+
+
+/*app.post('/videos/:id', (req,res)=>{
 
     const id = req.params.id;
     const name = req.body.name;
@@ -34,15 +48,16 @@ app.post('/videos/:id', (req,res)=>{
 
         res.status(418).send({message: 'We need a name'})
     }
+    if(!playlist){
+
+        res.status(418).send({message: 'We need a playlist'})
+    }
 
 
-    res.status(400).send({
-
-        name: `${name}`,
-        playlist: `${playlist}`
-    })
+    data.push(req.body);
+    res.json(data);
 })
-
+*/
 
 app.listen(PORT, ()=>{
 
